4cfc54ff419b4f690c9f6351a165ccbbbb6c6bfc
Jörg Hagmann <joerg.hagmann@computomics.com>
2019-11-25 22:01:35 +0100

    accelerated lookup of bin coverage between links in a path

--- matrixcomponent/JSONparser.py
+++ matrixcomponent/JSONparser.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from matrixcomponent.matrix import GraphPath, GraphBins
+from matrixcomponent.matrix import GraphPath
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,7 +10,6 @@ def parse(file):
             for line in f:
                 data.append(json.loads(line))
 
-        Bins = GraphBins()
         paths = []
 
         max_bin = 1
@@ -20,29 +19,32 @@ def parse(file):
 
                 p = GraphPath(path['path_name'])
 
-                # idx = 0
-                # for b in path['bins']:
-                #     Bins.bins[b[0]] =
-
-
-
-
+                # dummy first bin in p.bins:
                 prev_bin = p.Bin()
+                prev_bin.set(0, 0, 0, 0)
+                p.bins.append(prev_bin)
+
                 p.mapping_id2idx[0] = 0
-                idx = 0
+                idx = 1
                 for b in path['bins']:
+                    # store bin values in 'bin'
                     bin = p.Bin()
                     bin.set(b[0], b[1], b[2], b[3])
                     if b[0] > max_bin:
                         max_bin = b[0]
                     p.bins.append(bin)
+
+                    # set next_bin of the previous bin (prev_bin) to this bin:
                     prev_bin.next_bin = b[0]
                     prev_bin = bin
+
+                    # populate mapping betw binID and index in bin_set:
                     p.mapping_id2idx[b[0]] = idx
                     idx += 1
-                p.mapping_id2idx[prev_bin.bin_id] = idx-1
+                # last bin points to itself:
+                #p.mapping_id2idx[prev_bin.bin_id] = idx-1
                 prev_bin.next_bin = prev_bin.bin_id
-                p.bins.append(prev_bin)  # last bin in a GraphPath needs to indicate stop, max_bin?
+                #p.bins.append(prev_bin)
 
                 p.finalize_bins()
                 
